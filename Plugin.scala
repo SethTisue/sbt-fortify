7091dc8629c0967998b63f56b08b7ae14065165b
Seth Tisue <seth@tisue.net>
2017-08-16 17:47:46 -0700

    wip

--- Plugin.scala
+++ Plugin.scala
@@ -8,64 +8,10 @@ object FortifyPlugin extends AutoPlugin {
   override def requires = sbt.plugins.JvmPlugin
   override def trigger = allRequirements
 
-  // thank you http://stackoverflow.com/a/37271468/86485 !
-
-  object autoImport {
-    val translateCommand = Command.command("translate") {state =>
-      def settingValue[T](key: SettingKey[T]): T = {
-        val extracted: Extracted = Project.extract(state)
-        import extracted._
-        val thisScope = Load.projectScope(currentRef)
-          (key in thisScope get extracted.structure.data).get
-      }
-      val binaryVersion = settingValue(scalaBinaryVersion)
-      val targetDir = settingValue(target)
-      val compilerPluginJar = for {
-        (newState, result) <- Project.runTask(fullClasspath in Compile, state)
-        cp <- result.toEither.right.toOption
-        jar <- cp.find(
-          _.get(moduleID.key).exists{mId =>
-            println(mId)
-            mId.organization == "com.lightbend" &&
-              mId.name == s"scala-fortify_$binaryVersion"})
-      } yield jar.data.absolutePath
-      Project.runTask(clean in Compile, state)
-      Project.runTask(scalacOptions, state) match {
-        case Some((newState, result)) =>
-          result.toEither.right.foreach { defaultScalacOptions =>
-            Project.runTask(compile in Compile,
-              Project.extract(state).append(
-                scalacOptions := defaultScalacOptions ++ Seq[String](
-                  s"-Xplugin:${compilerPluginJar.get}",
-                  "-Ystop-before:jvm",
-                  "-Xplugin-require:fortify",
-                  s"-P:fortify:out=$targetDir"),
-                newState))}
-        case None =>
-          sys.error("Couldn't get default scalacOptions")
-      }
-      state
-    }
-
-    val scanCommand = Command.command("scan") {state =>
-      val fpr = "scan.fpr"
-      IO.delete(new java.io.File(fpr))
-      val targetDir = {
-        val extracted: Extracted = Project.extract(state)
-        import extracted._
-        val thisScope = Load.projectScope(currentRef)
-        (target in thisScope get extracted.structure.data).get
-      }
-      val nstFiles = (targetDir ** "*.nst").get.map(_.toString)
-      (Seq("sourceanalyzer", "-filter", "filter.txt", "-f", fpr, "-scan") ++ nstFiles).!
-      state
-    }
-  }
-
-  import autoImport._
-
   override lazy val projectSettings = Seq(
-    commands ++= Seq(translateCommand, scanCommand),
+    commands ++= Seq(
+      Command.command("translate")(translate),
+      Command.command("scan")(scan)),
     credentials += Credentials(Path.userHome / ".lightbend" / "commercial.credentials"),
     resolvers += Resolver.url(
       "lightbend-commercial-releases",
@@ -77,5 +23,57 @@ object FortifyPlugin extends AutoPlugin {
         exclude("com.typesafe.conductr", "ent-suite-licenses-parser")
         exclude("default", "scala-st-nodes")
   )
+
+  /// helpers
+
+  def settingValue[T](state: State, key: SettingKey[T]): T = {
+    val extracted: Extracted = Project.extract(state)
+    import extracted._
+    val thisScope = Load.projectScope(currentRef)
+      (key in thisScope get extracted.structure.data).get
+  }
+
+  def compilerPluginJar(state: State): Option[String] = {
+    val binaryVersion = settingValue(state, scalaBinaryVersion)
+    for {
+      (newState, result) <- Project.runTask(fullClasspath in Compile, state)
+      cp <- result.toEither.right.toOption
+      jar <- cp.find(
+        _.get(moduleID.key).exists(mId =>
+          mId.organization == "com.lightbend" &&
+            mId.name == s"scala-fortify_$binaryVersion"))
+    } yield jar.data.absolutePath
+  }
+
+  // thank you http://stackoverflow.com/a/37271468/86485 !
+  def translate(state: State): State = {
+    val targetDir = settingValue(state, target)
+    val moreOptions = Seq[String](
+      s"-Xplugin:${compilerPluginJar(state).get}",
+      "-Ystop-before:jvm",
+      "-Xplugin-require:fortify",
+      s"-P:fortify:out=$targetDir")
+    Project.runTask(clean in Compile, state)
+    Project.runTask(scalacOptions, state) match {
+      case Some((newState, result)) =>
+        result.toEither.right.foreach {options =>
+          Project.runTask(compile in Compile,
+            Project.extract(state).append(
+              scalacOptions := options ++ moreOptions,
+              newState))}
+      case None =>
+        sys.error("Couldn't get default scalacOptions")
+    }
+    state
+  }
+
+  def scan(state: State): State = {
+    val fpr = "scan.fpr"
+    IO.delete(new java.io.File(fpr))
+    val targetDir = settingValue(state, target)
+    val nstFiles = (targetDir ** "*.nst").get.map(_.toString)
+    (Seq("sourceanalyzer", "-filter", "filter.txt", "-f", fpr, "-scan") ++ nstFiles).!
+      state
+  }
 
 }
